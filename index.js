7c7b5394fcc96b3f3efe3c21e63bf66af80a78b2
diego amozoo <uzoma@diegos-MacBook-Pro-2.local>
2023-09-27 23:11:28 -0600

    installed jsmodbus, implemented dummy functions when modbus and bacnet buttons and sliders messages arrived

--- index.js
+++ index.js
@@ -10,7 +10,86 @@ server.listen(port, () => {
   console.log(`WebSocket server is running on port ${port}`);
 });
 
+/////////MODBUS/////////////
+const net = require('net');
+const modbus = require('jsmodbus');
+const netServer = new net.Server();
+const netPort = 502;
+const ModbusServer = new modbus.server.TCP(netServer, {
 
+});
+
+ModbusServer.on('connection', (client) => {
+  console.log('Modbus connection successful')
+});
+
+ModbusServer.on('postWriteMultipleRegisters', (value) => {
+  console.log('Write multiple registers: ',value._body)
+});
+
+netServer.listen(netPort, () => {
+  console.log(`NetServer is running on port ${netPort}`);
+});
+
+function modbusButton1() {
+  console.log("Modbus button 1");
+}
+
+function modbusButton2() {
+  console.log("Modbus button 2");
+}
+
+function modbusButton3() {
+  console.log("Modbus button 3");
+}
+
+function modbusButton4() {
+  console.log("Modbus button 4");
+}
+
+function modbusButton5() {
+  console.log("Modbus button 5");
+}
+
+function modbusSlider1(mbs){
+  console.log("Modbus Slider 1 = " + mbs);
+}
+
+function modbusSlider2(mbs){
+  console.log("Modbus Slider 2 = " + mbs);
+}
+
+/////////BACNET/////////////
+
+function bacnetButton1() {
+  console.log("Bacnet button 1");
+}
+
+function bacnetButton2() {
+  console.log("Bacnet button 2");
+}
+
+function bacnetButton3() {
+  console.log("Bacnet button 3");
+}
+
+function bacnetButton4() {
+  console.log("Bacnet button 4");
+}
+
+function bacnetButton5() {
+  console.log("Bacnet button 5");
+}
+
+function bacnetSlider1(bns){
+  console.log("Bacnet Slider 1 = " + bns);
+}
+
+function bacnetSlider2(bns){
+  console.log("Bacnet Slider 2 = " + bns);
+}
+
+//////////////////////////////
 const clients = {};
 const users = {};
 let userActivity = [];
@@ -41,6 +120,59 @@ function handleMessage(message, userId) {
   else{
     const dataFromClient = JSON.parse(message.toString());
     console.log(dataFromClient)
+    ///////////MODBUS///////////////
+    if (dataFromClient.modbusButton){
+      switch (dataFromClient.modbusButton) {
+        case 1 :
+          modbusButton1();
+          break;
+        case 2 :
+          modbusButton2();
+          break;
+        case 3 :
+          modbusButton3();
+          break;
+        case 4 :
+          modbusButton4();
+          break;
+        case 5 :
+          modbusButton5();
+          break;
+      }
+    }
+    else if (dataFromClient.type == "modbusSlider1") {
+      modbusSlider1(dataFromClient.modbusSlider);
+    }
+    else if (dataFromClient.type == "modbusSlider2") {
+      modbusSlider2(dataFromClient.modbusSlider);
+    }
+    ///////////BACNET///////////////
+    else if (dataFromClient.bacnetButton){
+      switch (dataFromClient.bacnetButton) {
+        case 1 :
+          bacnetButton1();
+          break;
+        case 2 :
+          bacnetButton2();
+          break;
+        case 3 :
+          bacnetButton3();
+          break;
+        case 4 :
+          bacnetButton4();
+          break;
+        case 5 :
+          bacnetButton5();
+          break;
+      }
+    }
+    else if (dataFromClient.type == "bacnetSlider1") {
+      bacnetSlider1(dataFromClient.bacnetSlider);
+    }
+    else if (dataFromClient.type == "bacnetSlider2") {
+      bacnetSlider2(dataFromClient.bacnetSlider);
+    }
+    
     broadcastMessage(dataFromClient, userId);
   }
   
